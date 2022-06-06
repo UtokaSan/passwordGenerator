5d79bf34517700a56a26d6cab69a13b5dc403f3a
Florian Boulbes <74573533+UtokaSan@users.noreply.github.com>
2022-06-06 14:59:26 +0200

    [ADD] Files with function generator & length
    This JS file is a generator of password with a length

---
+++ passwordGenerator.js
@@ -0,0 +1,36 @@
+const inputGenerator = document.querySelector("#passwordGenerator");
+const inputNumber = document.querySelector("#passwordNumber");
+const inputSymbols = document.querySelector("#passwordSymbols");
+const length = document.querySelector("#length");
+
+const result = document.querySelector(".result1");
+
+const letter = "abcdefghijkmnopqrstuwxyzABCDEFGHIJKMNOPQRSTUWXYZ";
+const numbers = "0123456789";
+const symbols = "?/^$%#~*£"; 
+
+function generatorPassword (str, nb) {
+    let result = "";
+
+    for (i = 0; i < nb; i++) {
+        result = result + str.charAt(Math.floor(Math.random() * str.length)); 
+    }
+    return result;
+}
+
+
+inputGenerator.addEventListener("click", () => {
+    let alpha = letter;
+
+    inputNumber.checked ? (alpha = alpha + numbers) : "";
+    inputSymbols.checked ? (alpha = alpha + symbols) : "";
+
+    if (length.value == typeof '') {
+        return 0;
+    } else {
+        result.innerHTML = generatorPassword(str, length.value);
+    }
+});
+
+inputGenerator.value = "Generator";
+length.value = "Number";
